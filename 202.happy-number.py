07445c388b22892d3a78b1a93009f65fc06974a2
sarthak-sb <sarthak@smartbooks.jp>
2022-07-31 14:35:26 +0530

    https://leetcode.com/problems/happy-number/

---
+++ 202.happy-number.py
@@ -0,0 +1,26 @@
+# @before-stub-for-debug-begin
+from typing import *
+
+from python3problem202 import *
+
+# @before-stub-for-debug-end
+
+#
+# @lc app=leetcode id=202 lang=python3
+#
+# [202] Happy Number
+#
+
+# @lc code=start
+
+
+class Solution:
+    def isHappy(self, n: int) -> bool:
+        memo = set()
+        while n not in memo:
+            memo.add(n)
+            n = sum(int(i) ** 2 for i in str(n))
+        return 1 in memo
+
+
+# @lc code=end
