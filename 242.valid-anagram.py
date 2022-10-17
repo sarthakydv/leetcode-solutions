374a007bcbfe81b9e38f893f895958d462f29e18
sarthak-sb <sarthak@smartbooks.jp>
2022-10-17 13:42:46 +0530

    Add Pythonic way 3

--- 242.valid-anagram.py
+++ 242.valid-anagram.py
@@ -20,7 +20,15 @@ class Solution:
         # return not any(count)
 
         # Pythonic way 2
-        return collections.Counter(s) == collections.Counter(t)
+        # return collections.Counter(s) == collections.Counter(t)
+
+        # Pythonic way 3
+        # Not required here but is a more general solution
+        count = [0] * 27
+        for chr1, chr2 in zip_longest(s, t, fillvalue="{"):
+            count[ord(chr1) - ord("a")] += 1
+            count[ord(chr2) - ord("a")] -= 1
+        return not any(count)
 
 
 # @lc code=end
