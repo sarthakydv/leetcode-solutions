d576256c38225aca8341f7dab45f7f8656d58e92
sarthak-sb <sarthak@smartbooks.jp>
2022-10-17 13:35:59 +0530

    Create 242.valid-anagram.py

---
+++ 242.valid-anagram.py
@@ -0,0 +1,23 @@
+#
+# @lc app=leetcode id=242 lang=python3
+#
+# [242] Valid Anagram
+#
+
+# @lc code=start
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        if len(s) != len(t):
+            return False
+        # Using sorting
+        # return sorted(s) == sorted(t)
+
+        # Pythonic Way
+        count = [0] * 26
+        for chr1, chr2 in zip(s, t):
+            count[ord(chr1) - ord("a")] += 1
+            count[ord(chr2) - ord("a")] -= 1
+        return not any(count)
+
+
+# @lc code=end
