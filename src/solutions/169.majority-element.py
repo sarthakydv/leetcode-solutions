0d7e6713642c9b7d8e06556d88fdc09de6998a48
sarthakydv <sydv248@gmail.com>
2023-06-26 07:23:56 +0530

    Update 169.majority-element.py

--- src/solutions/169.majority-element.py
+++ src/solutions/169.majority-element.py
@@ -1,19 +1,24 @@
+# @before-stub-for-debug-begin
+from python3problem169 import *
+from typing import *
+# @before-stub-for-debug-end
+
 #
 # @lc app=leetcode id=169 lang=python3
 #
 # [169] Majority Element
 #
 
+
 # @lc code=start
 class Solution:
     def majorityElement(self, nums: List[int]) -> int:
-        dic = {}
-        for num in nums:
-            dic[num] = dic.get(num, 0) + 1
-
-        for num, cnt in dic.items():
-            if cnt > len(nums) // 2:
-                return num
+        count = {}
+        for n in nums:
+            count[n] = count.get(n, 0) + 1
+            if count[n] > len(nums) // 2:
+                return True
+        return False
 
 
 # @lc code=end
