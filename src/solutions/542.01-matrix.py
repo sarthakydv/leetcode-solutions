0d30200a1b28256dd81b0d6235d0bf5204575246
sarthakydv <sydv248@gmail.com>
2023-06-26 07:05:42 +0530

    Create 542.01-matrix.py

---
+++ src/solutions/542.01-matrix.py
@@ -0,0 +1,12 @@
+#
+# @lc app=leetcode id=542 lang=python3
+#
+# [542] 01 Matrix
+#
+
+# @lc code=start
+class Solution:
+    def updateMatrix(self, mat: List[List[int]]) -> List[List[int]]:
+        
+# @lc code=end
+
